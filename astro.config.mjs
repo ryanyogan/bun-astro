c58d0a803033a5adb484499f6a6ada3b69dd3045
Ryan Yogan <ryanyogan@gmail.com>
2023-09-10 22:35:10 -0500

    feat // init

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: vercel(),
+});
