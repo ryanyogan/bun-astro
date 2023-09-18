3d14375bc70d9f9dede1e9506e548bd16ba80a6a
Ryan Yogan <ryanyogan@gmail.com>
2023-09-18 11:03:24 -0500

    feat // prepping for becoming the new blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Ryan Yogan";
+export const SITE_DESCRIPTION =
+  "Software engineer and engineering leader with over 15 years of experience.";
